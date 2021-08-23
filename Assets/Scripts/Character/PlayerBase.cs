9f5256f61aabe1da4a2575725b9699a428997fc0
OnurBasci <onurbasc@gmail.com>
2021-08-23 19:51:55 +0200

    .12

--- Assets/Scripts/Character/PlayerBase.cs
+++ Assets/Scripts/Character/PlayerBase.cs
@@ -1,22 +1,42 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBase: MonoBehaviour
 {
+    [System.NonSerialized] public static PlayerBase referance;
     public static float health = 100;
+    private float temp;
+
+    private bool hit = false;
 
     private void Update()
     {
-        
+
+        OnHit();
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag.Equals("Thunder"))
+        if (other.tag.Equals("Obstacle"))
         {
             health -= 20;
-            Debug.Log("çat!");
+            Debug.Log("cat!");
+            hit = true;
+            temp = health - 20;
         }
+    }
+
+    public void OnHit()
+    {
+        if (hit)
+        {
+            health = Mathf.Lerp(health, health - 20, 0.05f);
+            if (health <= temp)
+            {
+                hit = false;
+            }
+        }
+
     }
 }
