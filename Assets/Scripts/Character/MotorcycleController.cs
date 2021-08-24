c14ddf484f0d31e05813809f138c6acac3714e3a
DeadZFrame <canaltay2001@windowslive.com>
2021-08-24 19:05:06 +0300

    Squashed commit of the following:
    commit e944f6b0130e21bb8f15cb678fd3a07edba25b95
    Author: DeadZFrame <canaltay2001@windowslive.com>
    Date:   Tue Aug 24 19:04:42 2021 +0300
    
        motor, car, speedometer

--- Assets/Scripts/Character/MotorcycleController.cs
+++ Assets/Scripts/Character/MotorcycleController.cs
@@ -17,13 +17,28 @@ public class MotorcycleController : MonoBehaviour
     public float breakForce;
     public float maxSteerAngle;
 
-    public GameObject FrontWheel, RearWheel, handleBar;
+    public Transform FrontWheel, RearWheel, handleBar;
+    public Transform frontRight, frontLeft, rearRight, rearLeft;
+    public WheelCollider frontRightCollider, frontLeftCollider, rearRightCollider, rearLeftCollider, frontCollider, rearCollider;
+
+    private Vector3 prevPos = new Vector3();
+    [System.NonSerialized]public float speedval = 0;
 
     private void FixedUpdate()
     {
         GetInput();
         Motor();
         HandleSteering();
+        UpdateWheels();
+    }
+
+    public void SpeedCalculation()
+    {
+        var posNow = transform.position;
+        var speed = (posNow - prevPos) / Time.fixedDeltaTime;
+        prevPos = posNow;
+
+        speedval = speed.magnitude;
     }
 
     public void GetInput()
@@ -35,28 +50,37 @@ public class MotorcycleController : MonoBehaviour
 
     public void Motor()
     {
-        FrontWheel.GetComponent<WheelCollider>().motorTorque = verticalInput * motorForce;
+        frontRightCollider.motorTorque = verticalInput * motorForce;
+        frontLeftCollider.motorTorque = verticalInput * motorForce;
+        frontCollider.motorTorque = verticalInput * motorForce;
         currentbreakForce = isBreaking ? breakForce : 0f;
         ApplyBreaking();
     }
 
     public void ApplyBreaking()
     {
-        FrontWheel.GetComponent<WheelCollider>().brakeTorque = currentbreakForce;
-        RearWheel.GetComponent<WheelCollider>().brakeTorque = currentbreakForce;
+        frontRightCollider.brakeTorque = currentbreakForce;
+        rearLeftCollider.brakeTorque = currentbreakForce;
+        rearRightCollider.brakeTorque = currentbreakForce;
+        frontLeftCollider.brakeTorque = currentbreakForce;
     }
 
     public void HandleSteering()
     {
         currentSteerAngle = maxSteerAngle * horizontalInput;
-        FrontWheel.GetComponent<WheelCollider>().steerAngle = currentSteerAngle;
+        frontRightCollider.steerAngle = currentSteerAngle;
+        frontLeftCollider.steerAngle = currentSteerAngle;
+        frontCollider.steerAngle = currentSteerAngle;
     }
 
     public void UpdateWheels()
     {
-        UpdateSingleWheel(FrontWheel.GetComponent<WheelCollider>(), FrontWheel.transform);
-        UpdateSingleWheel(RearWheel.GetComponent<WheelCollider>(), RearWheel.transform);
-        handleBar.transform.rotation = FrontWheel.transform.rotation;
+        UpdateSingleWheel(frontRightCollider, frontRight);
+        UpdateSingleWheel(rearLeftCollider, rearLeft);
+        UpdateSingleWheel(rearRightCollider, rearRight);
+        UpdateSingleWheel(frontLeftCollider, frontLeft);
+        UpdateSingleWheel(frontCollider, FrontWheel);
+        UpdateSingleWheel(rearCollider, RearWheel);    
     }
 
     private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
