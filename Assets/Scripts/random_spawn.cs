88100cc2a3f0462daa5cd0395cedcb344b1b41fe
OnurBasci <onurbasc@gmail.com>
2021-08-26 14:03:11 +0200

    .22

--- Assets/Scripts/random_spawn.cs
+++ Assets/Scripts/random_spawn.cs
@@ -59,7 +59,7 @@ public class random_spawn : MonoBehaviour
 
         for (float i = start_point.x + rand_vertical; i < end_point.x; i += rand_vertical)
         {
-            int rand_horizontal = Random.Range(-2, 3);
+            float rand_horizontal = Random.Range(-2f, 2f);
             rand_horizontal = rand_horizontal * horizantal_width;
             GameObject random_object = get_random_object(objects);
             Instantiate(random_object, new Vector3(i, random_object.transform.position.y, player.transform.position.z + rand_horizontal), Quaternion.identity);
